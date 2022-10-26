fcc57e8b95c0706a413804ff46e9c7833dc1689b
Admir Trakic <aty@codan.dk>
2022-10-26 14:32:12 +0000

    Update

--- src/build-local.sh
+++ src/build-local.sh
@@ -4,4 +4,4 @@ docker build -f ./Dockerfile -t "$img" .
 docker tag "$img":latest "$img":$(git rev-parse HEAD)
 docker image ls "$img"
 
-kubectl run -i --rm  --image=local/example:latest --restart=Never -- dotnet --info
+kubectl run demo -it --rm --image=local/example:latest --restart=Never --image-pull-policy=Never -- "dotnet --info"
