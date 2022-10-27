45aa6ec6217385b1b5fb6e5e799782370fe178a3
Admir Trakic <atrakic@users.noreply.github.com>
2022-10-27 07:52:08 +0200

    Update

--- src/build-local.sh
+++ src/build-local.sh
@@ -1,7 +1,15 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+set -o pipefail
+
+eval "$(minikube -p minikube docker-env)"
+
 img="local/example"
-
+tag="$(git rev-parse HEAD)"
 docker build -f ./Dockerfile -t "$img" .
-docker tag "$img":latest "$img":$(git rev-parse HEAD)
-docker image ls "$img"
-
-kubectl run demo -it --rm --image=local/example:latest --restart=Never --image-pull-policy=Never -- "dotnet --info"
+docker tag "$img":latest "$img":"$tag"
+docker image ls --no-trunc "$img"
+# kubectl run demo -it --rm --image="$img":"$tag" --restart=Never --image-pull-policy=Never -- "dotnet --info"
+eval "$(minikube docker-env --unset)"
